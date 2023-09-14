5393ec22c43a41b69cb7d2de33983b154d271947
Agustin Vallejo <122490326+agualbo79@users.noreply.github.com>
2023-09-13 21:12:56 -0300

    endpoint registro, run.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from api.datebase import DatabaseConnection
-from api.routes import auth_bp
+from api.routes.rutas_bp import usuario_bp  
 
 app = Flask(__name__)
 
@@ -9,11 +9,10 @@ def prueba_db():
     # Realizar una consulta de ejemplo a la base de datos
     query = "SELECT * FROM usuarios"
     resultado = DatabaseConnection.fetch_all(query)
-
     
     return str(resultado)
 
-app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(usuario_bp, url_prefix='/auth')  
 
 if __name__ == '__main__':
     app.run()
--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from app import init_app
+from app import app
 
 if __name__ == "__main__":
-    app = init_app()
     app.run()
